59827ad6dbdb231d8cc836c9f127fecae9b1f2b5
student <shaik.mohammadaabid@hcl.com>
2021-04-20 08:25:08 +0000

    FirstCommit

---
+++ HelloDevOps/src/main/java/com/aabid/Graded/HelloDevOps/HelloDevOpsApplication.java
@@ -0,0 +1,14 @@
+package com.aabid.Graded.HelloDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDevOpsApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ HelloDevOps/src/test/java/com/aabid/Graded/HelloDevOps/HelloDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aabid.Graded.HelloDevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
